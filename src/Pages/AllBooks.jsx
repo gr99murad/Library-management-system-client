1392a3df664062d7940737870ebe61eef7a3bcc7
gr99murad <golamrabbanimurad@gmail.com>
2025-01-14 20:28:05 +0600

    Fetch to all books data in all books route

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactStars from 'react-rating-stars-component';
+import { Link } from 'react-router-dom';
 
 const AllBooks = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/books')
+        .then(res => res.json())
+        .then(data => {
+            setBooks(data);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error(error);
+            setError('Failed to load books data');
+            setLoading(false);
+        });
+    }, []);
+
+    if(loading){
+        return <div>Loading...</div>
+    }
+    if(error){
+        return <div>{error}</div>
+    }
     return (
-        <div>
-            all books
-        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
+        {books.map(book => (
+            <div key={book._id} className="card bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src= {book.image}
+                alt={book.name}
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{book.name}</h2>
+              <p>Author: {book.author}</p>
+              <p>Category: {book.category}</p>
+              <p>Quantity: {book.quantity}</p>
+              <ReactStars count={5} value={book.rating} size={24} activeColor='#ffd700'></ReactStars>
+              <div className="card-actions">
+                <Link to={`/updateBook/${book._id}`}><button className="btn btn-primary" >Update</button></Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     );
 };
 
