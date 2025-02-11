dabff0da12177f314939f36cfa84d6c420dc007f
gr99murad <golamrabbanimurad@gmail.com>
2025-02-12 01:16:23 +0600

    update banner slider in background image and align problem solve

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -17,20 +17,40 @@ const Banner = () => {
    
     
     return (
-        <div className='max-w-screen-xl mx-auto px-4 py-20'>
+        <div className='max-w-screen-xl mx-auto px-7 md:px-6 lg:px-4 py-20'>
             <Slider {...setting}>
-                <div className='flex justify-center h-[300px] bg-gradient-to-br from-blue-500 via-cyan-400 to-blue-600 text-white text-center rounded-lg p-6 shadow-lg'>
-                    <h3 className='text-4xl font-bold mb-4 text-shadow-lg'>Welcome to Our Library</h3>
-                    <p className='text-lg max-w-4xl mx-auto leading-relaxed'>Explore a world of knowledge with our extensive collection of books across various genres.</p>
+                <div className='relative '>
+                    <img className='h-96  w-full object-cover' src="https://i.ibb.co.com/s9jCp8BL/handsome-guy-study-library-1.jpg" alt="Library" />
+                <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40 rounded-lg text-center text-white px-8 md:px-4 '>
+                    <div>
+                    <h3 className='text-2xl md:text-4xl font-bold mb-4 text-shadow-lg'>Welcome to Our Library</h3>
+                    <p className='text-sm md:text-lg max-w-4xl mx-auto leading-relaxed'>Explore a world of knowledge with our extensive collection of books across various genres.</p>
+                    </div>
                 </div>
-                <div className='flex justify-center h-[300px] bg-gradient-to-br from-green-500 via-cyan-400 to-green-600 text-white text-center rounded-lg p-6 shadow-lg'>
-                    <h3 className='text-4xl font-bold mb-4 text-shadow-lg'>New Arrivals</h3>
-                    <p className='text-lg max-w-4xl mx-auto leading-relaxed'>Check out the latest additions to our library and stay updated with trending titles.</p>
                 </div>
-                <div className='flex justify-center h-[300px] bg-gradient-to-br from-purple-500 via-cyan-400 to-purple-600 text-white text-center rounded-lg p-6 shadow-lg'>
-                    <h3 className='text-4xl font-bold mb-4 text-shadow-lg'>Join Our Community</h3>
-                    <p className='text-lg max-w-4xl mx-auto leading-relaxed'>Become a member today and enjoy exclusive benefits, including book reservation</p>
+
+                <div className='relative'>
+                    <img className='h-96 w-full object-cover' src="https://i.ibb.co.com/hTrVZ65/man-library-with-tablet.jpg" alt="New Arrivals" />
+                <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40 rounded-lg text-center text-white px-8 md:px-4 '>
+                    <div>
+                    <h3 className='text-2xl md:text-4xl font-bold mb-4 text-shadow-lg'>New Arrivals</h3>
+                    <p className='text-sm md:text-lg max-w-4xl mx-auto leading-relaxed'>Check out the latest additions to our library and stay updated with trending titles</p>
+                    </div>
                 </div>
+                </div>
+
+
+                <div className='relative'>
+                    <img className='h-96 w-full object-cover' src="https://i.ibb.co.com/GQcvMZRQ/9176169-6607.jpg" alt="Join Our Community" />
+                <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40 rounded-lg text-center text-white px-8 md:px-4 '>
+                    <div>
+                    <h3 className='text-2xl md:text-4xl font-bold mb-4 text-shadow-lg'>Join Our Community</h3>
+                    <p className='text-sm md:text-lg max-w-4xl mx-auto leading-relaxed'>Become a member today and enjoy exclusive benefits, including book reservation</p>
+                    </div>
+                </div>
+                </div>
+                
+
             </Slider>
         </div>
     );
